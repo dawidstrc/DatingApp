5b6acabaeaa4ca7b5e9e102b943bbc47ed239d42
Dawid Straciło <dawidstrc@gmail.com>
2021-03-07 22:21:30 +0100

    Building a walking skeleton - API
    Adding the first files of the created project.

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DatingApp
+Application with ASP.NET Core API (.Net 5.0) and Angular 10
