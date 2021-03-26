e77a119950e614d69ba4be5b970ab90a85b388fe
Dawid Straciło <dawidstrc@gmail.com>
2021-03-26 02:40:39 +0100

    Completing the walking skeleton - Angular
    I created a new Angular application in which I configured the HTTPS protocol.

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -31,6 +31,7 @@ namespace DatingApp
             });
             //services.AddRazorPages();
             services.AddControllersWithViews();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +52,8 @@ namespace DatingApp
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
